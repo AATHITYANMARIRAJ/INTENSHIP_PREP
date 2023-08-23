baba37315a54815221e2d2b7d280ab2e5e8de7eb
AATHITYAN MARIRAJ <114569274+AATHITYANMARIRAJ@users.noreply.github.com>
2023-08-23 07:36:09 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cout<<"enter the string\n";
+    getline(cin,s);
+    stringstream ss(s);
+    int count=0;
+    int line=0;
+    string word;
+    while(ss >> word){
+        if(word[i])
+    }
+    cout<<"the no of lines are"<<line<<endl;
+    return 0;
+}
